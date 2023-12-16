93ae15736601332fabe060c7c7ad69d966920092
timliftiev <tliftiev2@gmail.com>
2023-12-16 18:11:05 +0200

    initail java back setup

---
+++ src/main/java/com/stucoursered/javacourseprojectback/JavaCourseProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.stucoursered.javacourseprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourseProjectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stucoursered/javacourseprojectback/JavaCourseProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stucoursered.javacourseprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
